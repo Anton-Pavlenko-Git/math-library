3411cad0cc9d2cfdbd89aef828f99a23e3b582c2
Anton-Pavlenko-Git <anton.pavlenko.85@gmail.com>
2025-11-27 21:54:53 +0300

    Create math_utils.hpp

---
+++ include/math_utils/math_utils.hpp
@@ -0,0 +1,40 @@
+# pragma once
+
+namespace math_utils {
+  inline int add(int a, int b) {
+    return a + b;
+  }
+
+  inline int subtract(int a, int b) {
+    return a - b;
+  }
+
+  inline int multiply(int a, int b) {
+    return a * b;
+  }
+  
+  inline int devide(int a, int b) {
+    if(b == 0) {
+      throw std::runtime_error("Division by zero!");
+    }
+    return a / b;
+  }
+  
+  inline long long power(int base, int exponent) {
+    if(base == 0)     return 0;
+    if(exponent == 0) return 1;
+
+    long long result = 1;
+    for(int i = 0; i < exponent; ++i) {
+      result *= base;
+    }
+    return result;
+  }
+
+  inline long long factorial(int n) {
+    if(n < 0) return -1;
+    if(n == 0 || n == 1) return 1;
+
+    return n*factorial(n - 1);
+  }
+}
