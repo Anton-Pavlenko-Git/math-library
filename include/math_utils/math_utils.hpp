f82fd493765d6a7f4864b13f54292c8443ef740c
Anton-Pavlenko-Git <anton.pavlenko.85@gmail.com>
2025-12-06 11:38:29 +0300

    Remove std::cout from math_utils.hpp + .clang-format style

--- include/math_utils/math_utils.hpp
+++ include/math_utils/math_utils.hpp
@@ -1,42 +1,35 @@
-# pragma once
+#pragma once
 
 namespace math_utils {
-  inline int add(int a, int b) {
-    return a + b;
-  }
+inline int add(int a, int b) { return a + b; }
 
-  inline int subtract(int a, int b) {
-    return a - b;
-  }
+inline int subtract(int a, int b) { return a - b; }
 
-  inline int multiply(int a, int b) {
-    return a * b;
-  }
-  
-  inline int divide(int a, int b) {
+inline int multiply(int a, int b) { return a * b; }
+
+inline int divide(int a, int b) {
     if(b == 0) {
-      std::cout << "Division by zero!\n";
-      return -1;
+        return -1;
     }
     return a / b;
-  }
-  
-  inline int power(int base, int exponent) {
-    if(base == 0)     return 0;
+}
+
+inline int power(int base, int exponent) {
+    if(base == 0) return 0;
     if(exponent == 0) return 1;
-    int exp = std::abs(exponent);
-    
+    if(exponent < 0) exponent = -exponent;
+
     int result = 1;
-    for(int i = 0; i < exp; ++i) {
-      result *= base;
+    for(int i = 0; i < exponent; ++i) {
+        result *= base;
     }
     return result;
-  }
+}
 
-  inline long long factorial(int n) {
+inline long long factorial(int n) {
     if(n < 0) return -1;
     if(n == 0 || n == 1) return 1;
 
-    return n*factorial(n - 1);
-  }
+    return n * factorial(n - 1);
 }
+}  // namespace math_utils
